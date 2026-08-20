37107e8c2a34b2c5818c0b71c295b39c21c1d11a
Ishwari Dhale <ishwarinandrajj@gmail.com>
2026-08-21 00:00:06 +0530

    Day04 Leetcode Fibonacci problem

---
+++ 02-First-java/Fibonnaci2.java
@@ -0,0 +1,15 @@
+class Fibonacci2 {
+    public int fib(int n) {
+       if(n==0){
+        return 0;
+       }
+       else if(n==1){
+        return 1;
+       } 
+       else{
+        int result =fib(n-1)+fib(n-2);
+        return result;
+
+       }
+    }
+}
